1592740b018b9f65df1061900df8b4c6fe61efd7
KrisNathan <kristophernathanael2005@gmail.com>
2025-10-19 00:04:32 +0700

    refactor: restructure HashResult class and simplify hash building process

--- algorithms/hash/rolling_hash/rolling_hash.cpp
+++ algorithms/hash/rolling_hash/rolling_hash.cpp
@@ -8,45 +8,45 @@ using namespace std;
 
 typedef unsigned long long ull;
 
-struct HashResult {
-  vector<ull> hashes;
-  vector<ull> powers;
-};
-
-HashResult build_hash(string s, ull base, ull mod) {
-  int len = (int)s.size();
-  vector<ull> hashes(len + 1, 0);
-  vector<ull> powers(len + 1, 1);
-
-  for (int i = 1; i <= len; ++i) {
-    powers[i] = (powers[i - 1] * base) % mod;
-    hashes[i] = (hashes[i - 1] * base + (unsigned char)s[i - 1]) % mod;
-  }
-
-  return {hashes, powers};
-}
-
-ull substring_hash(int l, int r, ull mod, const vector<ull> &powers,
-                   const vector<ull> &hashes) {
-  if (l > r)
-    return 0;
-  return (hashes[r] + mod - (hashes[l - 1] * powers[r - l + 1]) % mod) % mod;
-}
-
 const ull base = 131ULL;
 const ull mod = 1000000007ULL; // 1e9+7
+
+class HashResult {
+public:
+  vector<ull> hashes;
+  vector<ull> powers;
+
+  HashResult(const vector<ull> &hashes, const vector<ull> &powers)
+      : hashes(hashes), powers(powers) {}
+
+  HashResult(const string &s, ull base, ull mod) {
+    int len = (int)s.size();
+    hashes = vector<ull>(len + 1, 0);
+    powers = vector<ull>(len + 1, 1);
+
+    for (int i = 1; i <= len; ++i) {
+      powers[i] = (powers[i - 1] * base) % mod;
+      hashes[i] = (hashes[i - 1] * base + (unsigned char)s[i - 1]) % mod;
+    }
+  }
+
+  ull substring_hash(int l, int r, ull mod) const {
+    if (l > r)
+      return 0;
+    return (hashes[r] + mod - (hashes[l - 1] * powers[r - l + 1]) % mod) % mod;
+  }
+};
 
 /**
  * O(n)
  */
 bool substring_search(string str, string query) {
-  HashResult shr = build_hash(str, base, mod);
-  HashResult qhr = build_hash(query, base, mod);
+  HashResult shr = HashResult(str, base, mod);
+  HashResult qhr = HashResult(query, base, mod);
 
   bool found = false;
   for (int i = 1; i + query.length() - 1 <= str.length(); i++) {
-    ull sub_hash =
-        substring_hash(i, i + query.length() - 1, mod, shr.powers, shr.hashes);
+    ull sub_hash = shr.substring_hash(i, i + query.length() - 1, mod);
     found = sub_hash == qhr.hashes[query.length()];
   }
 
@@ -54,9 +54,9 @@ bool substring_search(string str, string query) {
 }
 
 int main() {
-  bool found = substring_search("hello world", "world");
-
-  printf("is found? %d\n", found ? 1 : 0);
+  printf("is found? %d\n", substring_search("hello world", "world") ? 1 : 0);
+  printf("is found? %d\n", substring_search("hello world", "llo") ? 1 : 0);
+  printf("is found? %d\n", substring_search("hello world", "llow") ? 1 : 0);
 
   return 0;
 }
